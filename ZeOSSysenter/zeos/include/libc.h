37de3930ee94c7a0e778fd0604aa8acf4221e5be
pparri <stillalive.8811@gmail.com>
2024-11-22 12:57:42 +0100

    init

---
+++ ZeOSSysenter/zeos/include/libc.h
@@ -0,0 +1,34 @@
+/*
+ * libc.h - macros per fer els traps amb diferents arguments
+ *          definició de les crides a sistema
+ */
+ 
+#ifndef __LIBC_H__
+#define __LIBC_H__
+
+#include <stats.h>
+
+extern int errno;
+
+int write(int fd, char *buffer, int size);
+
+void itoa(int a, char *b);
+
+int strlen(char *a);
+
+void perror();
+
+int getpid();
+
+int fork();
+
+void exit();
+
+int yield();
+
+int get_stats(int pid, struct stats *st);
+
+void SAVE_REGS(void);
+void RESTORE_REGS(void);
+
+#endif  /* __LIBC_H__ */
